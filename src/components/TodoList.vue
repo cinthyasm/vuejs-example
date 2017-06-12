90fa9a93b3181af092bc0ddfecbd78aa34371b81
Dilan Quesada Rojas <di.quesada93@gmail.com>
2017-06-12 16:33:51 -0600

    Filter actions

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,23 +2,28 @@
   <div class="todo-list">
     <div class="container">
       <div class="row">
-          <div class="col-md-3">
-            <div class="todos__block">
-                <ul class="todos__list">
-                    <li class="todos__item" v-for="(todo,index) in todos" :class="{ 'todos__completed': todo.completed }" @click="todo.completed = !todo.completed" transition="fade"> 
-                    <span class="todos__delete" @click.stop="removeTodo(index)">
-                        <i class="fa fa-times" aria-hidden="true"></i></span>{{ todo.text }}
-                    </li>
-                </ul>
-            </div>
-          </div>
-          <div class="col-md-9">
-              <h2>New Task</h2>
-              <p>Type a task and press enter</p>
-            <span v-show="itemsTodo.length">{{ itemsTodo.length }} pending</span>
-            <input class="form-control" type="text" @keyup.enter="addTodo" v-model="newTodo"/>
+        <div class="col-md-2 sidebar aligner">
+          <ul class="tags"> 
+            <li @click="getAll">All</li>
+            <li @click="getActive">Active</li>
+            <li @click="getCompletes">Completed</li>
             <button class="btn todos__clear" @click.prevent="clearCompleted" v-show="itemsDone.length">Clear completed tasks</button>
+          </ul>
+        </div>
+        <div class="col-md-10">
+          <h2>New Task</h2>
+          <p>Type a task and press enter</p>
+          <span v-show="itemsTodo.length">{{ itemsTodo.length }} pending</span>
+          <input class="form-control" type="text" @keyup.enter="addTodo" v-model="newTodo"/>
+          <div class="todos__block">
+            <ul class="todos__list">
+                <li class="todos__item" v-for="(todo,index) in filteredList" :class="{ 'todos__completed': todo.completed }" @click="todo.completed = !todo.completed" transition="fade"> 
+                <span class="todos__delete" @click.stop="removeTodo(index)">
+                    <i class="fa fa-times" aria-hidden="true"></i></span>{{ todo.text }}
+                </li>
+            </ul>
           </div>
+        </div>
       </div>
     </div>
   </div>
@@ -30,19 +35,20 @@ export default {
   name: 'todo-list',
   data () {
     return {
-        title: 'Todo List',
-        newTodo: '',
-        todos: [
-            {text: 'drink coffee', completed: true},
-            {text: 'eat breakkie', completed: false},
-            {text: 'check e-mails', completed: false},
-            {text: 'hang in slack', completed: false},
-            {text: 'do some work', completed: false},
-            {text: 'check twitter', completed: false},
-            {text: 'seriously dude, work!', completed: false},
-            {text: 'drink coffee', completed: false},
-            {text: 'play in CodePen', completed: false}
-        ]
+      title: 'Todo List',
+      newTodo: '',
+      filteredList: [],
+      todos: [
+          {text: 'drink coffee', completed: true},
+          {text: 'eat breakkie', completed: false},
+          {text: 'check e-mails', completed: false},
+          {text: 'hang in slack', completed: false},
+          {text: 'do some work', completed: false},
+          {text: 'check twitter', completed: false},
+          {text: 'seriously dude, work!', completed: false},
+          {text: 'drink coffee', completed: false},
+          {text: 'play in CodePen', completed: false}
+      ]
     }
   },
   computed: {
@@ -54,7 +60,6 @@ export default {
     }
   },
    methods: {
-    
     addTodo() {
       // create new todo object
       const newEntry = {
@@ -69,24 +74,48 @@ export default {
         this.newTodo = '' 
       }
     },
-    
+    getCompletes(){
+      this.filteredList =  this.todos.filter(todo => todo.completed);
+    },
+    getActive(){
+      this.filteredList = this.todos.filter(todo => !todo.completed);
+    },
     removeTodo(index) {
       // remove todo item
       this.todos.splice(index, 1)
     },
-    
+    getAll(){
+      this.filteredList = this.todos;
+    },
     clearCompleted() {
       // only keep todo items not completed (see computed data)
-      this.todos = this.itemsTodo
+      this.filteredList = this.itemsTodo
     }
+  },//methods
+  created(){
+    this.filteredList = this.todos;
   }
   
 }
 </script>
 <style>
+
+
+.sidebar{
+  height: calc(100vh - 70px);
+  border-right: 1px solid #CCC;
+}
+
+.tags{
+  padding: 50px 0 0 0; 
+  list-style-type: none;
+}
+.tags li{
+  font-size: 3rem;
+  text-align: left;
+}
 .todos__block{
-    border-right: 1px solid #CCC;
-    height: 100vh;
+  height: 100vh;
 }
 .todos__title {
   margin-bottom: 1rem;
